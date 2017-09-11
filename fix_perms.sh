4a31914628b8b89a0784943f91a0aa962d15fbfb
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2017-09-11 16:46:23 +0200

    fix permission when shared dirs are mounted volumes

--- fix_perms.sh
+++ fix_perms.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Make sure at least the shared folders are writable by galaxy user
+chmod 777 /apollo-data/
+chmod 777 /tripal-data/
+chmod 777 /jbrowse/data/
+
+# Check permissions recursively and regularly if asked
 while [[ $ENABLE_FIX_PERMS == "1" ]]; do
     chmod 777 -R /apollo-data/
     chmod 777 -R /tripal-data/
